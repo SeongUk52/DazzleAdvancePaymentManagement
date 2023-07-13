aa4fd902767ec706ed981cfdd23898515861606c
정성욱 <rkfka7849@gmail.com>
2023-07-13 16:55:50 +0900

    프로젝트 개시
    스프링부트 프로젝트 생성

---
+++ DazzleAdvancePaymentManagement/src/test/java/com/DazzleAdvancePaymentManagement/DazzleAdvancePaymentManagement/DazzleAdvancePaymentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DazzleAdvancePaymentManagement.DazzleAdvancePaymentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DazzleAdvancePaymentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
