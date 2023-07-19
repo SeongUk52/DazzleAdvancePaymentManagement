679dde2a20a18f09db46eee95e227e8831c86598
정성욱 <rkfka7849@gmail.com>
2023-07-19 10:40:00 +0900

    v.0.2.2
    메뉴 추가기능 구현

--- DazzleAdvancePaymentManagement/src/main/java/com/DazzleAdvancePaymentManagement/DazzleAdvancePaymentManagement/goods/GoodsController.java
+++ DazzleAdvancePaymentManagement/src/main/java/com/DazzleAdvancePaymentManagement/DazzleAdvancePaymentManagement/goods/GoodsController.java
@@ -2,6 +2,7 @@ package com.DazzleAdvancePaymentManagement.DazzleAdvancePaymentManagement.goods;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.data.domain.Page;
@@ -20,5 +21,16 @@ public class GoodsController {
         Page<Goods> paging = this.goodsService.getList(page);
         model.addAttribute("paging", paging);
         return "goods_list";
+    }
+
+    @GetMapping("/create")
+    public String goodsCreate(){
+        return "goods_form";
+    }
+
+    @PostMapping("/create")
+    public String goodsCreate(@RequestParam String name, @RequestParam String category, @RequestParam Boolean ice, @RequestParam Integer amount, @RequestParam Integer price){
+        this.goodsService.createNewGoods(name,category,ice,amount,price);
+        return "redirect:/goods/list";
     }
 }
--- DazzleAdvancePaymentManagement/src/main/java/com/DazzleAdvancePaymentManagement/DazzleAdvancePaymentManagement/goods/GoodsService.java
+++ DazzleAdvancePaymentManagement/src/main/java/com/DazzleAdvancePaymentManagement/DazzleAdvancePaymentManagement/goods/GoodsService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import java.time.LocalDateTime;
 
 import java.util.List;
 
@@ -17,5 +18,15 @@ public class GoodsService {
     public Page<Goods> getList(int page) {
         Pageable pageable = PageRequest.of(page, 15);
         return this.goodsRepository.findAll(pageable);
+    }
+    public void createNewGoods(String name, String category, Boolean ice, Integer amount, Integer price) {
+        Goods g = new Goods();
+        g.setGoodsName(name);
+        g.setGoodsCategory(category);
+        g.setGoodsIce(ice);
+        g.setGoodsAmount(amount);
+        g.setGoodsPrice(price);
+        g.setGoodsDate(LocalDateTime.now());
+        this.goodsRepository.save(g);
     }
 }
